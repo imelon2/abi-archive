f3d835ee7dec1a8c5ad94480ea0feb32bef54833
imelon2 <imelon279@naver.com>
2025-05-20 10:50:24 +0900

    save abi archive

--- script/generate.ts
+++ script/generate.ts
@@ -6,61 +6,42 @@ import { glob } from "glob";
 import { run } from "hardhat";
 import { ethers, hexlify, id, toBeHex } from "ethers";
 import { encodeErrorResult, parseAbiItem, parseAbiParameter } from "viem";
-
-const abiFile = "+([a-zA-Z0-9_]).json";
-const solFile = "+([a-zA-Z0-9_-]).sol";
-
-const strip0x = (hex: string) => {
-  return hex.startsWith("0x") ? hex.slice(2) : hex;
-};
-
-const save = (selector: string, item: any) => {
-  const archivePath = path.join(__dirname, `../archive`);
-
-  console.log(selector);
-
-  // console.log(`archivePath: ${archivePath}`);
-
-  const hexSelector = strip0x(selector);
-  const index1 = hexSelector.slice(0, 2);
-  const index2 = hexSelector.slice(2, 4);
-  const filePath = `${archivePath}/${index1}/${index2}`;
-  const filePathAbi = `${filePath}/abi.json`;
-
-  if (!fs.existsSync(filePath)) {
-    fs.mkdirSync(filePath, { recursive: true });
-  }
-
-  if (!fs.existsSync(filePathAbi)) {
-    fs.writeFileSync(filePathAbi, JSON.stringify([item], null, 2));
-  } else {
-    console.log("multi depth!!  " + filePathAbi);
-    
-    const origin = fs.readFileSync(filePathAbi, "utf8");
-    const data = JSON.parse(origin)
-    data.push(item)
-    
-    // @TODO: origin에 중복되면 저장x
-    fs.writeFileSync(
-      filePathAbi,
-      JSON.stringify(data, null, 2)
-    );
-  }
-};
+import { strip0x } from "../src/utils/utils";
+import {
+  errorPath,
+  eventPath,
+  functionPath,
+  solFile,
+} from "../src/utils/constant";
 
 async function main() {
   const cwd = process.cwd();
-  const abiPath = path.join(cwd, `build`);
-
   const openzeppelin = path.join(
     cwd,
     `node_modules/@openzeppelin/contracts/**/**/${solFile}`
   );
   const allFiles = await glob([openzeppelin]);
+  const compiled = solcCompile(allFiles);
 
+  const contracts = compiled.contracts;
+  seperate(contracts);
+
+  console.log("done.");
+
+  //   console.log(compiled);
+  //   console.log(compiled.errors[0].sourceLocation);
+
+  // console.log(JSON.stringify(compiled.contracts['/Users/choi/Documents/me/choiRepo/abi-archive/node_modules/@openzeppelin/contracts/access/AccessControl.sol'],null,2));
+
+  // await run("compile", {
+  // sources: [solPath],
+  //   });
+}
+
+const solcCompile = (globedSolPath: string[]) => {
   let sources: any = {};
 
-  allFiles.map((sol) => {
+  globedSolPath.map((sol) => {
     const code = fs.readFileSync(sol, "utf8");
     sources[sol] = {
       content: code,
@@ -78,39 +59,65 @@ async function main() {
       },
     },
   };
-  const compiled = JSON.parse(solc.compile(JSON.stringify(input)));
-  const contracts = compiled.contracts;
+  return JSON.parse(solc.compile(JSON.stringify(input)));
+};
+
+const seperate = (contracts: any) => {
   Object.keys(contracts).map((contractPath) => {
     Object.keys(contracts[contractPath]).map((contractName) => {
       const { abi } = contracts[contractPath][contractName];
 
       for (const item of abi) {
-        // if (item.type === "error" || item.type === "function") {
         if (item.type === "function") {
           const types = item.inputs.map((i: any) => i.type).join(",");
           const signature = `${item.name}(${types})`; // name([types...])
           const selector = id(signature).slice(0, 10); // 0x + 4 bytes
 
-          console.log(`${signature} => ${selector}`);
-          save(selector, item);
+          save(selector, item, functionPath);
+        } else if (item.type === "error") {
+          const types = item.inputs.map((i: any) => i.type).join(",");
+          const signature = `${item.name}(${types})`; // name([types...])
+          const selector = id(signature).slice(0, 10); // 0x + 4 bytes
+
+          save(selector, item, errorPath);
+        } else if (item.type === "event") {
+          const types = item.inputs.map((i: any) => i.type).join(",");
+          const signature = `${item.name}(${types})`; // name([types...])
+          const topic = id(signature);
+
+          save(topic, item, eventPath);
         }
       }
     });
   });
+};
 
-  // fs.writeFileSync(
-  //   abiPath + "/" + c + ".json",
-  //   JSON.stringify(contracts[contract][c].abi, null, 2)
-  // );
-  console.log("done.");
+const save = (selector: string, item: any, archivePath: string) => {
+  // design trie-based layout directory storage
+  const hexSelector = strip0x(selector);
+  const index1 = hexSelector.slice(0, 2);
+  const index2 = hexSelector.slice(2, 4);
+  const filePath = `${archivePath}/${index1}/${index2}`;
+  const filePathAbi = `${filePath}/abi.json`;
 
-  //   console.log(compiled);
-  //   console.log(compiled.errors[0].sourceLocation);
+  if (!fs.existsSync(filePath)) {
+    fs.mkdirSync(filePath, { recursive: true });
+  }
 
-  // console.log(JSON.stringify(compiled.contracts['/Users/choi/Documents/me/choiRepo/abi-archive/node_modules/@openzeppelin/contracts/access/AccessControl.sol'],null,2));
+  if (!fs.existsSync(filePathAbi)) {
+    fs.writeFileSync(filePathAbi, JSON.stringify([item], null, 2));
+  } else {
+    const origin = fs.readFileSync(filePathAbi, "utf8");
+    const data = JSON.parse(origin);
+    const isDuplicate = data.some(
+      (existing: any) => JSON.stringify(existing) === JSON.stringify(item)
+    );
 
-  // await run("compile", {
-  // sources: [solPath],
-  //   });
-}
+    if (!isDuplicate) {
+      data.push(item);
+      fs.writeFileSync(filePathAbi, JSON.stringify(data, null, 2));
+    }
+  }
+};
+
 void main();
