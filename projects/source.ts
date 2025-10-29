26bb949b8666e47649e184b6e75ea567a1d4a27b
imelon2 <imelon279@naver.com>
2025-10-29 15:03:43 +0900

    add zmeamil UniversalEmailRecoveryModule.sol

--- projects/source.ts
+++ projects/source.ts
@@ -7,9 +7,10 @@ export const genSource = async () => {
   const cwd = process.cwd();
   const gnosisSafeL2 = path.join(cwd, `source/GnosisSafe/GnosisSafeL2.json`);
   const relayRouter = path.join(cwd, `source/RelayRouter/RelayRouter.json`);
+  const zkEmail_UniversalEmailRecoveryModule = path.join(cwd, `source/ZkEmail/UniversalEmailRecoveryModule.json`);
   
   
-  const allFiles = await glob([gnosisSafeL2,relayRouter]);
+  const allFiles = await glob([gnosisSafeL2,relayRouter,zkEmail_UniversalEmailRecoveryModule]);
 
   seperateByJson(allFiles);
 }
