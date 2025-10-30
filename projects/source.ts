deefb68b760dc74566912eafb57ab02dc0688220
imelon2 <imelon279@naver.com>
2025-10-30 16:03:40 +0900

    add zmEmail/email-recovery

--- projects/source.ts
+++ projects/source.ts
@@ -1,16 +1,21 @@
 import * as path from 'path';
-import { glob } from 'glob';
+import { glob, sync } from 'glob';
 import { seperateByJson } from '../src/utils/archive';
+import { abiFile, ignore } from '../src/utils';
 
 
 export const genSource = async () => {
   const cwd = process.cwd();
   const gnosisSafeL2 = path.join(cwd, `source/GnosisSafe/GnosisSafeL2.json`);
   const relayRouter = path.join(cwd, `source/RelayRouter/RelayRouter.json`);
-  const zkEmail_UniversalEmailRecoveryModule = path.join(cwd, `source/ZkEmail/UniversalEmailRecoveryModule.json`);
+  const zkEmail = sync(path.join(cwd, `source/ZkEmail/**/**/${abiFile}`),{
+      ignore:ignore
+    });
   
   
-  const allFiles = await glob([gnosisSafeL2,relayRouter,zkEmail_UniversalEmailRecoveryModule]);
+  
+  
+  const allFiles = await glob([gnosisSafeL2,relayRouter]);
 
-  seperateByJson(allFiles);
+  seperateByJson([...allFiles,...zkEmail]);
 }
